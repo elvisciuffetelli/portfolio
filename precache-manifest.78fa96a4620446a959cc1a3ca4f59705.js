e0f2662ecefe0307894bd35ac7bc785b704b6c8a
Elvis Ciuffetelli <ciuffetellielvis@gmail.com>
2021-03-07 01:12:28 +0100

    Updates

---
+++ precache-manifest.78fa96a4620446a959cc1a3ca4f59705.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18c92fd8fcf441efbabb33454b01980b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a41fb8d25220b5971ef",
+    "url": "/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "e8e0e7516f2f0dc04e64",
+    "url": "/static/css/4.3e7d09e6.chunk.css"
+  },
+  {
+    "revision": "acf53a6dc2c867fca8b4",
+    "url": "/static/css/main.db7bd74d.chunk.css"
+  },
+  {
+    "revision": "05c0c3aa2c0e1b262984",
+    "url": "/static/js/2.ca872a95.chunk.js"
+  },
+  {
+    "revision": "0a6d64cd3b49be6a14afdf21fa7c6d43",
+    "url": "/static/js/2.ca872a95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a41fb8d25220b5971ef",
+    "url": "/static/js/3.69bc2eaf.chunk.js"
+  },
+  {
+    "revision": "e8e0e7516f2f0dc04e64",
+    "url": "/static/js/4.4618d923.chunk.js"
+  },
+  {
+    "revision": "acf53a6dc2c867fca8b4",
+    "url": "/static/js/main.52922426.chunk.js"
+  },
+  {
+    "revision": "3ee7927a2635f523bef4",
+    "url": "/static/js/runtime-main.06a6b828.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "6eb0d3c0c99fa3ae7d49ed7e16411c2f",
+    "url": "/static/media/abaqLogo.6eb0d3c0.png"
+  },
+  {
+    "revision": "e281c834c06bb164522cfec065407aba",
+    "url": "/static/media/aesysLogo.e281c834.png"
+  },
+  {
+    "revision": "481db3f6fbe47ae91308c9f56bfadfad",
+    "url": "/static/media/avanadeLogo.481db3f6.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "35ef5e915631be2c31ccdff55b53db29",
+    "url": "/static/media/harvardLogo.35ef5e91.png"
+  },
+  {
+    "revision": "368dca84c786e3e126e9337b66e41326",
+    "url": "/static/media/illimityLogo.368dca84.webp"
+  },
+  {
+    "revision": "5cc53be040fd37f99ff0447eb4484a33",
+    "url": "/static/media/logo-eni.5cc53be0.webp"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "e53c65af79f8fa6b77e7f680ae3fea8f",
+    "url": "/static/media/smeg.e53c65af.webp"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.510f212ffbf261350b78d1e3fbecb400.js"
+  "/precache-manifest.78fa96a4620446a959cc1a3ca4f59705.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
