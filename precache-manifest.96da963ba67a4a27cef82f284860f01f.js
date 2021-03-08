7b98667a8f21b629b4216452ebf869c5174f8345
elvisciuffetelli <elvisciuffetelli@users.noreply.github.com>
2021-03-08 12:01:41 +0000

    Deploying to gh-pages from  @ 3c55b75dd6539c8d34f85c53cf9e439c89daa3bd 🚀

---
+++ precache-manifest.96da963ba67a4a27cef82f284860f01f.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df374af9092dfccd398fc163d2b08fa2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fe5e8ebb095c1c8be80",
+    "url": "/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "5ba7bbb0e7423ac372af",
+    "url": "/static/css/4.3e7d09e6.chunk.css"
+  },
+  {
+    "revision": "5482bf19eab4fe57fdbb",
+    "url": "/static/css/main.db7bd74d.chunk.css"
+  },
+  {
+    "revision": "b646d9fa50b10d7e5d68",
+    "url": "/static/js/2.3a919fe2.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.3a919fe2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fe5e8ebb095c1c8be80",
+    "url": "/static/js/3.8450745a.chunk.js"
+  },
+  {
+    "revision": "5ba7bbb0e7423ac372af",
+    "url": "/static/js/4.9a7e9aef.chunk.js"
+  },
+  {
+    "revision": "5482bf19eab4fe57fdbb",
+    "url": "/static/js/main.b7da7d67.chunk.js"
+  },
+  {
+    "revision": "9b84a49d4c5d400c7a8b",
+    "url": "/static/js/runtime-main.7d7a4348.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "6eb0d3c0c99fa3ae7d49ed7e16411c2f",
+    "url": "/static/media/abaqLogo.6eb0d3c0.png"
+  },
+  {
+    "revision": "e281c834c06bb164522cfec065407aba",
+    "url": "/static/media/aesysLogo.e281c834.png"
+  },
+  {
+    "revision": "481db3f6fbe47ae91308c9f56bfadfad",
+    "url": "/static/media/avanadeLogo.481db3f6.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "35ef5e915631be2c31ccdff55b53db29",
+    "url": "/static/media/harvardLogo.35ef5e91.png"
+  },
+  {
+    "revision": "368dca84c786e3e126e9337b66e41326",
+    "url": "/static/media/illimityLogo.368dca84.webp"
+  },
+  {
+    "revision": "5cc53be040fd37f99ff0447eb4484a33",
+    "url": "/static/media/logo-eni.5cc53be0.webp"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "e53c65af79f8fa6b77e7f680ae3fea8f",
+    "url": "/static/media/smeg.e53c65af.webp"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
